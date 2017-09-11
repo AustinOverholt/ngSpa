31d4ba72c721d830d550c978f5e232aa266388ee
Austin Overholt <austinoverholt@gmail.com>
2017-09-11 14:31:55 -0700

    Added angular controller, service and app Added scripts to layout

---
+++ Web/Scripts/mainController.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('mainApp')
+        .controller('mainController', mainController);
+
+    mainController.$inject = ['$scope', 'mainService'];
+
+    function mainController($scope, mainService) {
+
+        var vm = this;
+        vm.$scope = $scope;
+        vm.mainService = mainService;
+        vm.$onInit = _init;
+        vm.people = [
+            { firstName: "Austin", lastName: "Overholt" },
+            { firstName: "Allie", lastName: "Overholt" }
+        ];
+
+
+        // The fold 
+
+        function _init() {
+            console.log('controller initialized');
+        }
+
+    }
+
+
+})();
