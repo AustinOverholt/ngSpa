f7fd1c8969bc016c5d508bab5f7a4711a45554b1
Austin Overholt <austinoverholt@gmail.com>
2017-09-12 13:06:29 -0700

    Got ui routing example working

--- Web/Scripts/mainApp.js
+++ Web/Scripts/mainApp.js
@@ -1,5 +1,26 @@
 ﻿(function () {
     'use strict';
     angular
-        .module('mainApp', []);
+        .module('mainApp', ['ui.router'])
+        .config(function ($stateProvider) {
+
+            $stateProvider
+
+                .state('home', {
+                    url: '/home',
+                    template: '<h1>Hello World</h1>'
+                    //templateUrl: 'Views/Partial/partial-home.html'
+                })
+
+                .state('about', {
+                    url: '/about',
+                    templateUrl: 'partial-about.html'
+                })
+
+                .state('contact', {
+                    url: '/contact',
+                    templateUrl: 'partial-contact.html'
+                });
+
+        });
 })();
