c8bfaedff75d343aa96be460ab1a4141271b7bc0
Austin Overholt <austinoverholt@gmail.com>
2017-09-13 17:24:24 -0700

    Added Model for Users

---
+++ Model/Domain/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngSpa.Model
+{
+    public class Users
+    {
+        public string Email { get; set; }
+        public int EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public int PhoneNumberConfirmed { get; set; }
+        public int TwoFactorEnabled { get; set; }
+        public DateTime LockoutEndDateUtc { get; set; }
+        public int LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string UserName { get; set; }
+    }
+}
