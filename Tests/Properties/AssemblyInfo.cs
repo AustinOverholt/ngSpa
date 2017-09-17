c4a5a142b79ab62a0eb03ac8a7a2ac6b2168abf5
Austin Overholt <austinoverholt@gmail.com>
2017-09-17 16:18:06 -0700

    Added unit tests for service. Working!

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ngSpa.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ngSpa.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84750477-c446-4c1e-b67c-49987bf0afbf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Services/UserServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ngSpa.Services;
+using ngSpa.Model;
+using System.Collections.Generic;
+
+namespace ngSpa.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void UserService_SelectAll_Test()
+        {
+            UserService svc = new UserService();
+            List<Users> model = svc.SelectAll();
+            Assert.IsNotNull(model);
+
+        }
+    }
+}
