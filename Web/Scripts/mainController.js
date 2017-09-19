549d549db2d82ba606ad9ddadfac0b02ccd3183e
Austin Overholt <austinoverholt@gmail.com>
2017-09-19 12:09:26 -0700

    "get" working on page load

--- Web/Scripts/mainController.js
+++ Web/Scripts/mainController.js
@@ -13,6 +13,7 @@
         vm.mainService = mainService;
         vm.$onInit = _init;
         vm.person = { firstName: "Austin", lastName: "Overholt" };
+        vm.users = [];
            
         // The fold 
 
@@ -24,6 +25,19 @@
         function _getUsers() {
             console.log("get users called");
             // on page load get list of users
+            mainService.get("/api/users/")
+                .then(_getUsersSuccess)
+                .catch(_getUsersFailed);
+
+            function _getUsersSuccess(resp) {
+                console.log("Get users success", resp);
+                vm.users = resp.data.Items;
+                console.log(vm.users);
+            }
+
+            function _getUsersFailed(err) {
+                console.log("Get users failed", err);
+            }
         }
     }
 })();
