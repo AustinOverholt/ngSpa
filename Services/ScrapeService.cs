3b986024c75248da1b17ba00551721fad8ac6113
Austin Overholt <austinoverholt@gmail.com>
2017-09-20 13:33:48 -0700

    Added scrape service for later

---
+++ Services/ScrapeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngSpa.Services
+{
+    public class ScrapeService
+    {
+        // Placeholder Service for web scraping
+        // Create Crud for storing scraped html into DB
+        // Html Agility pack for scraping and filtering
+    }
+}
