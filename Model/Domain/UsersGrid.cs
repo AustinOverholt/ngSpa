a1e0834b3ac184a95d595cf10ae4013886eaf71e
Austin Overholt <austinoverholt@gmail.com>
2017-09-21 11:07:05 -0700

    Added domain for grid

---
+++ Model/Domain/UsersGrid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngSpa.Model.Domain
+{
+    public class UsersGrid
+    {
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        public List<Users> data { get; set; }
+    }
+}
