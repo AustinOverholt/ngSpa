431bf18ba8b7d8050c439fe9e9cbdbd9184a6f1c
Austin Overholt <austinoverholt@gmail.com>
2017-09-21 20:55:21 -0700

    Added web scraper Added unit test for scraper Both working (tweak to include attr param)

--- Services/ScrapeService.cs
+++ Services/ScrapeService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,21 @@ namespace ngSpa.Services
 {
     public class ScrapeService
     {
-        // Placeholder Service for web scraping
-        // Create Crud for storing scraped html into DB
-        // Html Agility pack for scraping and filtering
+        // pass in link to be parsed and 
+        public List<string> ParseHtml(string html, string keyword)
+        {
+
+            var document = new HtmlWeb().Load(html + keyword);
+            var urls = document.DocumentNode.Descendants("img")
+                                            .Select(e => e.GetAttributeValue("src", null))
+                                            .Where(s => !String.IsNullOrEmpty(s));
+            List<string> list = urls.ToList();
+            List<string> newList = new List<string>();
+            foreach (var item in list)
+            {
+                newList.Add(item);
+            }
+            return newList;
+        }
     }
 }
---
+++ Tests/Services/ScrapeServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ngSpa.Services;
+using System.Collections.Generic;
+
+namespace ngSpa.Tests.Services
+{
+    [TestClass]
+    public class ScrapeServiceTest
+    {
+        [TestMethod]
+        public void ScrapeServiceMain()
+        {
+            ScrapeService svc = new ScrapeService();
+            List<string> html = svc.ParseHtml("https://www.wikipedia.org/", "");
+            Assert.IsNotNull(html);
+        }
+    }
+}
