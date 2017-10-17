306fb1e7939e29aa3331d2c1805e8db86a4ded6b
Austin Overholt <austinoverholt@gmail.com>
2017-10-17 01:43:25 -0700

    Got directives example working on home page

---
+++ Web/Scripts/Directives/userInfoCard.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module("mainApp")
+        .directive("userInfoCard", userInfoCard);
+
+    function userInfoCard() {
+        return {
+            template: "User Info Card",
+            restrict: "E"
+        }
+    }
+})();
