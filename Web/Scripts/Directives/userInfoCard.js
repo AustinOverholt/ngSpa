4d834db5601ffd5ed82cc4bde0a2b61acf473e27
Austin Overholt <austinoverholt@gmail.com>
2017-10-18 12:18:26 -0700

    Added a collapse feature for panel

--- Web/Scripts/Directives/userInfoCard.js
+++ Web/Scripts/Directives/userInfoCard.js
@@ -9,7 +9,10 @@
             templateUrl: "/Scripts/Directives/userInfoCard.html", // takes HTML from template
             restrict: "E", // restricts to element type
             controller: function ($scope) { // controller for directive functionality
-
+                $scope.collapsed = false; // by default panel is not collapsed
+                $scope.collapse = function () {
+                    $scope.collapsed = !$scope.collapsed; // when heading clicked panel collapses
+                } 
             }
         }
     }
